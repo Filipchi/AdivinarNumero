26092bb48076608dc37e89af86defe6c0406046e
Filipchi <jhonfredy_1r9@utp.edu.co>
2021-10-31 19:35:58 -0500

    avance bg-botones

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// var btn = document.getElementsByClassName(".btn"),
+//     pulso = document.getElementById("1")
+
+// function cambio(){
+//   pulso.classList.add("pulso");
+// }
+
+// btn.addEventListener('click', cambio)
+
+
+// -------------reiniciar----------------
+// --------------------------------------
+
+// Cambiar el texto de intentos restantes
+// var intentos = "Te quedan x intentos";
+// document.getElementById("intentos").innerHTML = intentos
