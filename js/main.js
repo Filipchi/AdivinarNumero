666afaff8bb4f00cd1424f1220b436c6480d8ca9
Filipchi <jhonfredy_1r9@utp.edu.co>
2021-11-01 23:28:27 -0500

    implementación logica en Js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
+// ---------------------aleatorio---------------------
+function aleatorio(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
 
-// var btn = document.getElementsByClassName(".btn"),
-//     pulso = document.getElementById("1")
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+numReal = aleatorio(1, 10);
+console.log(numReal);
+// ---------------------------------------------------
 
-// function cambio(){
-//   pulso.classList.add("pulso");
-// }
-
-// btn.addEventListener('click', cambio)
+var caja = document.getElementsByClassName('btn');
+var parr = document.getElementById('parrafo');
 
 
-// -------------reiniciar----------------
-// --------------------------------------
+for (let i = 0; i < caja.length; i++) {
+    caja[i].addEventListener('click', function () {
+        texto = caja[i].value;
+        console.log(texto);
+        parr.innerText = texto;
+        
+        caja[i].classList.add('pulso');
+        caja[i].disabled = true;
+
+        if (numReal > caja[i].value) {
+            console.log('El número seleccionado es menor al esperado');
+        } else if (numReal < caja[i].value) {
+            console.log('El número seleccionado es mayor al esperado');
+        } else {
+            console.log('Felicidades diste con el número correcto');
+        }
+    })
+
+}
 
 // Cambiar el texto de intentos restantes
 // var intentos = "Te quedan x intentos";
